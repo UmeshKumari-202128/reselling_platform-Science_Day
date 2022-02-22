13e3cfff6d63f9ac68eaf0c3244bc8cf62c349e6
UmeshKumari-202128 <84262718+UmeshKumari-202128@users.noreply.github.com>
2022-02-22 20:16:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "./Components/Footer";
+import ContactUs from "./Components/ContactUs";
 
 import "./App.css";
 
 function App() {
-  return (
-    <Footer/>
+  return (<>
     
+    <ContactUs/>
+    <Footer/></>
   );
 }         
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 import { GrInstagram } from "react-icons/gr";
 import { BsFacebook } from "react-icons/bs";
 import { FaTwitterSquare } from "react-icons/fa";
-import {FaGooglePlay } from "react-icons/fa";
-import {MdEmail } from "react-icons/md";
-import {AiOutlineCopyrightCircle} from "react-icons/ai";
+import { FaGooglePlay } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { AiOutlineCopyrightCircle } from "react-icons/ai";
 
 import "./Footer.css";
 
@@ -20,40 +20,81 @@ function Footer() {
             <th>TRENDING LOCATIONS</th>
             <th>ABOUT US</th>
             <th>OLX</th>
-            <th >FOLLOW US</th>
+            <th>FOLLOW US</th>
           </tr>
           <tr>
             <td>Kolkata</td>
             <td>Kolkata</td>
-            <td><a href="#">Kolkata</a></td>
+            <td>
+              <a href="#">Kolkata</a>
+            </td>
 
-            <td><a href="#">Kolkata</a></td>
-            
+            <td>
+              <a href="#">Kolkata</a>
+            </td>
+            <td id="gap">
+              <a href="#">
+                <BsFacebook
+                  style={{ fontSize: "200%", color: "black", margin: "7px" }}
+                />
+              </a>
+              <br />
+              <a href="#">
+                <GrInstagram
+                  style={{ fontSize: "200%", color: "black", margin: "7px" }}
+                />
+              </a>
+              <br />
+              <a href="#">
+                <FaTwitterSquare
+                  style={{ fontSize: "200%", color: "black", margin: "7px" }}
+                />
+              </a>
+              <br />
+              <a href="#">
+                <MdEmail
+                  style={{ fontSize: "200%", color: "black", margin: "7px" }}
+                />
+              </a>
+            </td>
           </tr>
           <tr>
             <td>Kolkata</td>
             <td>Kolkata</td>
-            <td><a href="#">Kolkata</a></td>
-            <td><a href="#">Kolkata</a></td>
+            <td>
+              <a href="#">Kolkata</a>
+            </td>
+            <td>
+              <a href="#">Kolkata</a>
+            </td>
+            <td></td>
           </tr>
           <tr>
             <td>Kolkata</td>
             <td>Kolkata</td>
-            <td><a href="#">Kolkata</a></td>
+            <td>
+              <a href="#">Kolkata</a>
+            </td>
+            <td></td>
+            <td></td>
           </tr>
           <tr>
             <td>Kolkata</td>
             <td></td>
-            <td><a href="#">Kolkata</a></td>
-          </tr>
-          <tr>
-            <td>Kolkata</td>
+            <td>
+              <a href="#">Kolkata</a>
+            </td>
             <td></td>
-            <td><a href="#">Kolkata</a></td>
+            <td></td>
           </tr>
         </table>
+      
       </div>
-      <div className="last">Central University of Haryana,<AiOutlineCopyrightCircle/> 2022</div>
+      <div className="last">
+       
+       Central University of Haryana,
+        <AiOutlineCopyrightCircle /> 2022
+      </div>
     </div>
   );
 }
